5f432f62c63766404f37ada9430e30c52e15b4b2
celadaalejandro <celadaalejandro@gmail.com>
2016-02-19 00:45:17 +0100

    Revert "Primer intento"
    This reverts commit 4f62c1b96f5a0d0f06daa6eb3cef204469fbe153.

--- udp/socket.h
+++
@@ -1,7 +0,0 @@
-void recvfrom_socket(int,struct sockaddr_in*);
-void  sendto_socket(int,struct sockaddr_in*);
-void fill(struct sockaddr_in, int);
-int open_socket();
-int closed_socket(int);
-int bind_socket(int, struct sockaddr_in*, int);
-
